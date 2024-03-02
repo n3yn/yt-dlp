0d592ffb07bb4381a28f4bced9ac1e4bc8dc855d
n3yn <161895244+n3yn@users.noreply.github.com>
2024-03-02 18:43:43 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,23 +1,29 @@
+from flask import Flask, request, jsonify, render_template
 from yt_dlp import YoutubeDL
-from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/')
 def index():
-	url = None
-	if request.method == 'POST':
-		url = request.form['url']
-		ydl_opts = {
-			"quiet":    True,
-			"simulate": True,
-			"forceurl": True,
-			'format': 'best'
-		}
-	with YoutubeDL(ydl_opts) as ydl:
-		info = ydl.extract_info(url)
-		url = info["url"]
-  return render_template('index.html', url=url)
+    return render_template('index.html')
+
+@app.route('/convert_url', methods=['POST'])
+def convert_url():
+    data = request.get_json()
+    url = data['url']
+    
+    ydl_opts = {
+        "quiet":    True,
+        "simulate": True,
+        "forceurl": True,
+        'format': 'best'
+    }
+    
+    with YoutubeDL(ydl_opts) as ydl:
+        info = ydl.extract_info(url)
+        url2 = info["url"]
+    
+    return jsonify({'url2': url2})
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0',debug=True)
+    app.run(debug=True)
