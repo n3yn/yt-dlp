e9d9f9ec18194192c80fad6db02a1fb36b72b2cc
n3yn <161895244+n3yn@users.noreply.github.com>
2024-06-15 22:28:22 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,38 +2,39 @@ from flask import Flask, request, jsonify
 from bs4 import BeautifulSoup
 import urllib.parse
 import requests
+import bs4
 
 app = Flask(__name__)
 
 @app.route('/search', methods=['POST'])
 def search():
-    data = request.json
-    query = data.get('query')
-
-    if not query:
-        return jsonify({'error': 'No query provided'}), 400
-
+    # Get the query from the client
+    query = request.form['query']
+    
+    # Encode the query for the URL
+    query = urllib.parse.quote(query)
+    
+    # Set the base domain
+    domain = 'https://www.melon365.com'
+    
+    # Construct the search URL
+    url = domain + '/video/search?sk=' + query
+    
+    # Make the request and parse the HTML
+    res = requests.get(url)
+    r = bs4.BeautifulSoup(res.text, "html.parser")
+    
+    # Extract the titles and URLs
     m_titles = []
     m_urls = []
-
-    try:
-        encoded_query = urllib.parse.quote(query)
-        domain = 'https://www.melon365.com'
-        url = domain + '/video/search?sk=' + encoded_query
-        res = requests.get(url)
-        res.raise_for_status()
-        
-        r = BeautifulSoup(res.text, "html.parser")
-
-        content_div = r.find_all('div', class_='newslistsearchtextrighttitle')
-        for content in content_div:
-            a = content.find('a')
-            m_urls.append(domain + a.get('href').split('.html')[0] + '.html')
-            m_titles.append(content.text.replace('\n', '').replace('\t', ''))
-
-        return jsonify({'titles': m_titles, 'urls': m_urls})
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
+    content_div = r.find_all('div', class_='newslistsearchtextrighttitle')
+    for content in content_div:
+        a = content.find('a')
+        m_urls.append(domain + a.get('href').split('.html')[0] + '.html')
+        m_titles.append(content.text.replace('\n', '').replace('\t', ''))
+    
+    # Return the results as JSON
+    return jsonify({'m_titles': m_titles, 'm_urls': m_urls})
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(debug=True)
